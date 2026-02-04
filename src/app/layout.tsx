a664c29879e0dd282282e8483957789f60cf3778
Marcus Cevolela <marcus.cevolela@gmail.com>
2026-02-04 09:44:23 -0300

    Re-Deploy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import { Cabecalho } from "@/components/header";
 
 export const metadata: Metadata = {
   title: "Pré-Venda | Márcio de Jagun | Litteris Editora",
-  description: "Pré-Venda do livro Filosofia Descolonial Do Candomblé Nagô do autor Márcio de Jagun - Litteris Editora ",
+  description: "Pré-Venda do livro Filosofia Descolonial Do Candomblé Nagô do autor Márcio de Jagun -- Litteris Editora ",
 };
 
 export default function RootLayout({
